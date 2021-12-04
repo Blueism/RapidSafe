ec647e61d1641e71d065cfaadcf884c6deeb4ed7
Blueism Laboratories <92654084+Blueism@users.noreply.github.com>
2021-12-04 13:17:56 -0500

    Instructions

--- Instructions (READ ME).lua
+++ Instructions (READ ME).lua
@@ -1,3 +1,13 @@
 -- 1. Place this script in StarterPlayerScripts for it to work.
 -- 2. Go into your game and open Developer Console - Navigation: Menu < Settings < Developer Console.
 -- 3. Once you have done this it should say STUDS is activated if it does your system is ready to go.
+
+-- RapidSafeGui should go in ScreenGui make sure it's disabled it will be worked on soon, it's located in GUIs.
+
+--[[
+
+In Scanning you will see
+< AntiExploit < ExploitScan, place those in workspace
+
+"-- 3. Once you have done this it should say STUDS is activated if it does your system is ready to go." it should say activated ExploitScan if it doesn't please read how to
+report a vulnerability here: https://github.com/Blueism/RapidSafe/blob/main/SECURITY
