8857405e704f8c138660d97f576cb4627a7e53ce
Blueism <92654084+Blueism@users.noreply.github.com>
2021-12-04 22:38:22 -0500

    Add files via upload

---
+++ Server1.1.lua
@@ -0,0 +1,24 @@
+--[[
+    ____   __    __  __ ______ ____ _____  __  ___   __     ___     ____  _____       ____ _   __ ______
+   / __ ) / /   / / / // ____//  _// ___/ /  |/  /  / /    /   |   / __ )/ ___/      /  _// | / // ____/
+  / __  |/ /   / / / // __/   / /  \__ \ / /|_/ /  / /    / /| |  / __  |\__ \       / / /  |/ // /     
+ / /_/ // /___/ /_/ // /___ _/ /  ___/ // /  / /  / /___ / ___ | / /_/ /___/ /_    _/ / / /|  // /___ _ 
+/_____//_____/\____//_____//___/ /____//_/  /_/  /_____//_/  |_|/_____//____/( )  /___//_/ |_/ \____/(_)
+--------------------------------------------------------------------------------------------------------
+				              	Blueism Laboratories, INC.
+				  --------------------------------------------------------
+								"Securing Progression"
+				------------------------------------------------------------
+							V8ML, iFlorida_Enforcement
+					
+]]--
+
+game:GetService("ReplicatedStorage").MoneyGiver.OnServerEvent:Connect(function(Player)
+	Player:Kick()
+	print("!!! STUDS ACTIVATED. USER EXPLOIT DETECTED. BACK-UP NOTICE SYSTEMS ('ExploitScan') !!!")
+	print("User has exploited, BANNED. You are SAFE.")
+	print("USER EXPLOIT ('ExploitScan')")
+end)
+
+-- SYSKey: Bnwt66US1RpmA+vQiTVjNqEKFEVovEFHoUFSofDwHi6ZXPqXLsmSw95s6wLs5CI6LaDdEgBD7u7ryN19dpSS14SwDNGjIXOz36f8URdRqI6SCSBQBy1Kf3oCMVaFfBaskTZ2ZsmI90r5GlHQ6eXoG01MV3jrDb5oRyeDDFlrOlU=
+-- DO NOT DELETE, System Key - Nothing will work without it
