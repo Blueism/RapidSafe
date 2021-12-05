6fbf3aeba3697e168bb55b70d87a844c148a7c06
Blueism <92654084+Blueism@users.noreply.github.com>
2021-12-04 22:32:52 -0500

    Add files via upload

---
+++ RapidSafe Version 1.1.lua
@@ -0,0 +1,27 @@
+-- RAPIDSAFE MANUAL
+
+--[[
+-- SYSKey: Bnwt66US1RpmA+vQiTVjNqEKFEVovEFHoUFSofDwHi6ZXPqXLsmSw95s6wLs5CI6LaDdEgBD7u7ryN19dpSS14SwDNGjIXOz36f8URdRqI6SCSBQBy1Kf3oCMVaFfBaskTZ2ZsmI90r5GlHQ6eXoG01MV3jrDb5oRyeDDFlrOlU=
+-- DO NOT DELETE, System Key - Nothing will work without it
+
+**MIT License**
+
+Copyright (c) 2021 Blueism Laboratories - RapidSafe V.1.1
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
